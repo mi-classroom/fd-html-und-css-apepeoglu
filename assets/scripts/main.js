2ac0bd85a68e7b979c2925d58516a11ebd47bcff
apepeoglu <alara.pepeoglu@smail.th-koeln.de>
2024-04-26 21:54:31 +0200

    JS - Mobile Navigation hinzugefügt

---
+++ assets/scripts/main.js
@@ -0,0 +1,20 @@
+let mainContent = document.querySelector('main');
+const mainMenu = document.querySelector('.main-menu');
+const burgerMenu = document.querySelector('.burger-menu');
+burgerMenu.addEventListener('click', openMenu);
+
+function openMenu() {
+  if (mainMenu.classList.contains('is-active')) {
+    // Close the menu
+    mainMenu.classList.remove('is-active');
+    burgerMenu.classList.remove('icon--close');
+    burgerMenu.classList.add('icon--menu');
+    mainContent.style.opacity = 1; // Restore the content
+  } else {
+    // Open the menu
+    mainMenu.classList.add('is-active');
+    burgerMenu.classList.remove('icon--menu');
+    burgerMenu.classList.add('icon--close');
+    mainContent.style.opacity = 0.5; // Dim the content
+  }
+}
